2e827ec06b7f4dc4d25b62527ca5d4fba14529b7
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-28 04:06:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -24,5 +24,9 @@ end
 #  else value.is_a? even
   #  return true
   #end
-#end
-#prime?(4)
+
+  # Create a range of numbers from 2 to one less than the number in question (num - 1)
+  # Loop through that range
+  # Ask if the number in question (num) is cleanly divisible by each number in the range (n)
+  # If any one of those range numbers (n) creates a 0 remainder when the number in question (num) is divided by that number (n), the number in question (num) is not prime
+  # If at the end of the loop, none of the range numbers (n) created a 0 remainder, the number in question (num) is, indeed, prime
